619693416522f1791f74cf1ac6e303efeffc9074
Harsh Patel <harshp5@usf.edu>
2024-09-22 17:34:17 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,4 +1,4 @@
-//Task 1 Create an Employees Array of Employee Objects
+// Task 1 Create an Employees Array of Employee Objects
 let employees = [
     {name: "Doc", shifts: [{day: "Monday", hours: 8}, { day: "Thursday", hours: 6}]},
     {name: "Manny", shifts: [{day: "Tuesday", hours: 6}, { day: "Friday", hours: 8}]},
@@ -6,4 +6,13 @@ let employees = [
     {name: "Shack", shifts: [{day: "Tuesday", hours: 7}]},
 ];
 console.log(employees);
+
+// Task 2 Create a Function to Display Employee Shift Details
+function displayEmployeeShifts(employee) {
+console.log(`Employee Name: ${employee.name}`);
+employee.shifts.forEach(shift => {
+    console.log( `Day: ${shift.day}, Hours Worked: ${shift.hours}`)
+});
+}
+employees.forEach(displayEmployeeShifts);
 
